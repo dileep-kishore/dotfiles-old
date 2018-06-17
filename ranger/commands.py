3945dd5cf6cb0eaea13243600c0a58f81a2199ac
Dileep Kishore <k.dileep1994@gmail.com>
2018-06-17 18:20:27 -0400

    Updated ranger configuration from red_peep branch

--- ranger/commands.py
+++ ranger/commands.py
@@ -57,55 +57,127 @@ class my_edit(Command):
         # content of the current directory.
         return self._tab_directory_content()
 
-    class mkcd(Command):
-        """
-        :mkcd <dirname>
-        Creates a directory with the name <dirname> and enters it.
-        """
-        def execute(self):
-            from os.path import join, expanduser, lexists
-            from os import makedirs
-            import re
-            dirname = join(self.fm.thisdir.path, expanduser(self.rest(1)))
-            if not lexists(dirname):
-                makedirs(dirname)
-                match = re.search('^/|^~[^/]*/', dirname)
-                if match:
-                    self.fm.cd(match.group(0))
-                    dirname = dirname[match.end(0):]
-                for m in re.finditer('[^/]+', dirname):
-                    s = m.group(0)
-                    if s == '..' or (s.startswith('.') and not self.fm.settings['show_hidden']):
-                        self.fm.cd(s)
-                    else:
-                        ## We force ranger to load content before calling `scout`.
-                        self.fm.thisdir.load_content(schedule=False)
-                        self.fm.execute_console('scout -ae ^{}$'.format(s))
-            else:
-                self.fm.notify("file/directory exists!", bad=True)
-
-    class fzf_select(Command):
-        """
-        :fzf_select
-        Find a file using fzf.
-        With a prefix argument select only directories.
-        See: https://github.com/junegunn/fzf
-        """
-        def execute(self):
-            import subprocess
-            if self.quantifier:
-                # match only directories
-                command="find -L . \( -path '*/\.*' -o -fstype 'dev' -o -fstype 'proc' \) -prune \
-                -o -type d -print 2> /dev/null | sed 1d | cut -b3- | fzf +m"
-            else:
-                # match files and directories
-                command="find -L . \( -path '*/\.*' -o -fstype 'dev' -o -fstype 'proc' \) -prune \
-                -o -print 2> /dev/null | sed 1d | cut -b3- | fzf +m"
-            fzf = self.fm.execute_command(command, stdout=subprocess.PIPE)
-            stdout, stderr = fzf.communicate()
-            if fzf.returncode == 0:
-                fzf_file = os.path.abspath(stdout.decode('utf-8').rstrip('\n'))
-                if os.path.isdir(fzf_file):
-                    self.fm.cd(fzf_file)
+class mkcd(Command):
+    """
+    :mkcd <dirname>
+    Creates a directory with the name <dirname> and enters it.
+    """
+    def execute(self):
+        from os.path import join, expanduser, lexists
+        from os import makedirs
+        import re
+        dirname = join(self.fm.thisdir.path, expanduser(self.rest(1)))
+        if not lexists(dirname):
+            makedirs(dirname)
+            match = re.search('^/|^~[^/]*/', dirname)
+            if match:
+                self.fm.cd(match.group(0))
+                dirname = dirname[match.end(0):]
+            for m in re.finditer('[^/]+', dirname):
+                s = m.group(0)
+                if s == '..' or (s.startswith('.') and not self.fm.settings['show_hidden']):
+                    self.fm.cd(s)
                 else:
-                    self.fm.select_file(fzf_file)
+                    ## We force ranger to load content before calling `scout`.
+                    self.fm.thisdir.load_content(schedule=False)
+                    self.fm.execute_console('scout -ae ^{}$'.format(s))
+        else:
+            self.fm.notify("file/directory exists!", bad=True)
+
+from collections import deque
+fd_deq = deque()
+
+class fd_search(Command):
+    """:fd_search [-d<depth>] <query>
+
+    Executes "fd -d<depth> <query>" in the current directory and focuses the
+    first match. <depth> defaults to 1, i.e. only the contents of the current
+    directory.
+    """
+
+    def execute(self):
+        import subprocess
+        from ranger.ext.get_executables import get_executables
+        if not 'fd' in get_executables():
+            self.fm.notify("Couldn't find fd on the PATH.", bad=True)
+            return
+        if self.arg(1):
+            if self.arg(1)[:2] == '-d':
+                depth = self.arg(1)
+                target = self.rest(2)
+            else:
+                depth = '-d1'
+                target = self.rest(1)
+        else:
+            self.fm.notify(":fd_search needs a query.", bad=True)
+            return
+
+        # For convenience, change which dict is used as result_sep to change
+        # fd's behavior from splitting results by \0, which allows for newlines
+        # in your filenames to splitting results by \n, which allows for \0 in
+        # filenames.
+        null_sep = {'arg': '-0', 'split': '\0'}
+        nl_sep = {'arg': '', 'split': '\n'}
+        result_sep = null_sep
+
+        process = subprocess.Popen(['fd', result_sep['arg'], depth, target],
+                    universal_newlines=True, stdout=subprocess.PIPE)
+        (search_results, _err) = process.communicate()
+        global fd_deq
+        fd_deq = deque((self.fm.thisdir.path + os.sep + rel for rel in
+            sorted(search_results.split(result_sep['split']), key=str.lower)
+            if rel != ''))
+        if len(fd_deq) > 0:
+            self.fm.select_file(fd_deq[0])
+
+class fd_next(Command):
+    """:fd_next
+
+    Selects the next match from the last :fd_search.
+    """
+
+    def execute(self):
+        if len(fd_deq) > 1:
+            fd_deq.rotate(-1) # rotate left
+            self.fm.select_file(fd_deq[0])
+        elif len(fd_deq) == 1:
+            self.fm.select_file(fd_deq[0])
+
+class fd_prev(Command):
+    """:fd_prev
+
+    Selects the next match from the last :fd_search.
+    """
+
+    def execute(self):
+        if len(fd_deq) > 1:
+            fd_deq.rotate(1) # rotate right
+            self.fm.select_file(fd_deq[0])
+        elif len(fd_deq) == 1:
+            self.fm.select_file(fd_deq[0])
+
+class fzf_select(Command):
+    """
+    :fzf_select
+    Find a file using fzf.
+    With a prefix argument select only directories.
+    See: https://github.com/junegunn/fzf
+    """
+    def execute(self):
+        import subprocess
+        if self.quantifier:
+            # match only directories
+            command="find -L . \( -path '*/\.*' -o -fstype 'dev' -o -fstype 'proc' \) -prune \
+            -o -type d -print 2> /dev/null | sed 1d | cut -b3- | fzf +m"
+        else:
+            # match files and directories
+            command="find -L . \( -path '*/\.*' -o -fstype 'dev' -o -fstype 'proc' \) -prune \
+            -o -print 2> /dev/null | sed 1d | cut -b3- | fzf +m"
+        fzf = self.fm.execute_command(command, stdout=subprocess.PIPE)
+        stdout, stderr = fzf.communicate()
+        if fzf.returncode == 0:
+            fzf_file = os.path.abspath(stdout.decode('utf-8').rstrip('\n'))
+            if os.path.isdir(fzf_file):
+                self.fm.cd(fzf_file)
+            else:
+                self.fm.select_file(fzf_file)
