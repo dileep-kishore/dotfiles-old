8be36a207ecd8948e28ca2375af8baa0e87a6dfb
Dileep Kishore <k.dileep1994@gmail.com>
2018-10-11 19:48:49 -0400

    Update killer script

--- i3/killer.sh
+++ i3/killer.sh
@@ -1,12 +1,18 @@
 #!/bin/bash
 
+. ./password.sh
+
 while true
 do
     pkill firefox
     pkill palemoon
+    pkil google-chrome
+    pkill vivaldi
     pkill brave
     pkill midori
     pkill vlc
+    pkill qutebrowser
+    echo "$MY_PASSWORD" | sudo -S chattr +i /etc/resolv.conf
     sleep 10
 done
 
