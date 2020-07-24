ef90f8c5f8fbf78ea9d996a794a08ccbd6c201d9
Dileep Kishore <k.dileep1994@gmail.com>
2020-07-24 16:14:07 -0400

    Update: Toggl polybar script

--- i3/polybar/toggl-polybar.py
+++ i3/polybar/toggl-polybar.py
@@ -9,9 +9,10 @@ try:
     result = [s.strip() for s in output.split("\n")]
     title, *rest = result
     data = {r.split(":", 1)[0]: r.split(":", 1)[-1].strip() for r in rest}
+    short_title = f"{title[:20]}..."
     project = data["Project"].split("(")[0]
     duration = data["Duration"]
-    display = f" {project}: {duration}"
+    display = f" {short_title} -  {project}: {duration}"
 except:
     display = " Stopped!"
 
