6785cc3800a6863232ec7a8f139f3d0c3cd04abe
Dileep Kishore <k.dileep1994@gmail.com>
2020-07-25 08:40:34 -0400

    Update display format in toggl polybar script

--- i3/polybar/toggl-polybar.py
+++ i3/polybar/toggl-polybar.py
@@ -8,12 +8,16 @@ output = process.stdout.decode("utf-8")
 try:
     result = [s.strip() for s in output.split("\n")]
     title, *rest = result
+    title = title.split("#")[0].strip()
     data = {r.split(":", 1)[0]: r.split(":", 1)[-1].strip() for r in rest}
-    short_title = f"{title[:20]}..."
-    project = data["Project"].split("(")[0]
+    if len(title) > 20:
+        short_title = f"{title[:20]}..."
+    else:
+        short_title = title
+    project = data["Project"].split("(")[0].strip()
     duration = data["Duration"]
-    display = f" {short_title} -  {project}: {duration}"
+    display = f" {short_title} [{project}]: {duration}"
 except:
-    display = " Stopped!"
+    display = " Start tracking!"
 
 print(display)
